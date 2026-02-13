033741ec20daf59dc4cef8ff791855b9873dde31
Mark De Guzman <mdguzman@homewoodhealth.com>
2026-02-12 17:16:23 -0700

    Wire in BAT-WEB-008
    update comments

--- tests/bat_web_test.py
+++ tests/bat_web_test.py
@@ -18,7 +18,7 @@ CREDENTIALS = {
 }
 
 def test_bat_web_001(homeweb):
-    # 1: Navigate to Homeweb landing
+    # 1: Test - Navigate to Homeweb landing
     homeweb.navigate_landing()
     homeweb.set_landing(True)
     assert "homeweb" in homeweb.current_url.lower()
@@ -26,30 +26,28 @@ def test_bat_web_001(homeweb):
 
 def test_bat_web_002(homeweb):
     assert homeweb.is_landing()
-
-    # 1: Set up
     resources = homeweb.landing["elements"]["resources"]
     paths = homeweb.landing["paths"]["resources"]
 
-    # 2: Test - Resource 1
+    # 1: Test - Resource 1
     homeweb.click_element("xpath", resources["emotional_intelligence"])
     assert paths["emotional_intelligence"] in homeweb.current_url.lower()
     assert homeweb.wait_for_resource_content()
     homeweb.go_back()
 
-    # 3: Test - Resource 2
+    # 2: Test - Resource 2
     homeweb.click_element("xpath", resources["anxiety"])
     assert paths["anxiety"] in homeweb.current_url.lower()
     assert homeweb.wait_for_resource_content()
     homeweb.go_back()
 
-    # 4: Test - Resource 3
+    # 3: Test - Resource 3
     homeweb.click_element("xpath", resources["letting_go"])
     assert paths["letting_go"] in homeweb.current_url.lower()
     assert homeweb.wait_for_resource_content()
     homeweb.go_back()
 
-    # 5: Test - Resource 4
+    # 4: Test - Resource 4
     homeweb.click_element("xpath", resources["toolkit"])
     assert paths["toolkit"] in homeweb.current_url.lower()
     assert homeweb.wait_for_resource_content()
@@ -57,33 +55,32 @@ def test_bat_web_002(homeweb):
 
 def test_bat_web_003(homeweb, quantum):
     assert homeweb.is_landing()
-
-    # 1: Set up
     buttons = homeweb.landing["elements"]["buttons"];
     paths = homeweb.landing["paths"]["buttons"];
     inputs = quantum.login["elements"]["inputs"]
 
-    # 2: Test - Sign In
+    # 1: Test - Sign In - Button
     homeweb.click_element("xpath", buttons["sign_in"])
     assert paths["sign_in"] in quantum.current_url.lower()
 
-    # 3: Test - Email field
+    # 2: Test - Email field
     quantum.input(inputs["email_address"], CREDENTIALS["personal"]["email"])
     quantum.submit()
     password_input = quantum.wait_for_password()
     assert password_input.is_displayed()
 
-    # 4: Test - Password field
+    # 3: Test - Password field
     quantum.input(inputs["password"], CREDENTIALS["personal"]["password"])
     quantum.submit()
 
-    # 5: Test - Login
+    # 4: Test - Login
     assert homeweb.wait_for_dashboard()
     homeweb.set_authenticated(True)
 
 def test_bat_web_004(homeweb):
-    assert homeweb.is_authenticated
+    assert homeweb.is_authenticated()
 
+    # 1: Test - Navigate to resource
     resource_target = "https://homeweb.ca/en/user/articles/56252b81e40e6f50062aa714"
     homeweb.driver.get(resource_target)
     assert homeweb.wait_for_resource_content()
@@ -91,26 +88,26 @@ def test_bat_web_004(homeweb):
 def test_bat_web_005(homeweb):
     assert homeweb.is_authenticated
 
+    # 1: Navigate to sentio resource
     sentio_resource_target = "https://homeweb.ca/app/en/resources/62c5a1e929ed9c1608d0434b"
-    sentio_access_button = {
-        "identifier": homeweb.authenticated["elements"]["buttons"]["access_sentio"],
-        "path": homeweb.authenticated["paths"]["buttons"]["access_sentio"]
-    }
     homeweb.driver.get(sentio_resource_target)
     assert homeweb.wait_for_resource_content()
 
-    homeweb.click_element("xpath",sentio_access_button["identifier"])
+    # 2: Test - Sentio transfer kickout
+    homeweb.click_element("class name", "btn-primary")
     assert homeweb.wait_for_sentio_transfer()
     homeweb.go_back()
 
 def test_bat_web_006(homeweb):
-    assert homeweb.is_authenticated
-
+    assert homeweb.is_authenticated()
     header = homeweb.header
     header_buttons = header.elements["buttons"]
+
+    # 1: Test - Menu dropdown
     header.click_element("class name", header_buttons["menu"])
     assert header.wait_for_dropdown()
 
+    # 2: Test - Logout
     header.click_element("css selector", header_buttons["sign_out"])
     assert "https://homeweb.ca/" in homeweb.current_url.lower()
     assert homeweb.is_landing()
@@ -118,31 +115,56 @@ def test_bat_web_006(homeweb):
 
 def test_bat_web_007(homeweb, quantum):
     assert homeweb.is_landing()
-
     header = homeweb.header
     header_buttons = header.elements["buttons"]
     paths = header.paths["buttons"]
     inputs = quantum.login["elements"]["inputs"]
 
-    # 2: Test - Sign In
+    # 1: Test - Sign In - Header
     header.click_element("class name", header_buttons["sign_in"])
     assert paths["sign_in"] in quantum.current_url.lower()
 
-    # 3: Test - Email field
+    # 2: Test - Email field
     quantum.input(inputs["email_address"], CREDENTIALS["demo"]["email"])
     quantum.submit()
     password_input = quantum.wait_for_password()
     assert password_input.is_displayed()
 
-    # 4: Test - Password field
+    # 3: Test - Password field
     quantum.input(inputs["password"], CREDENTIALS["demo"]["password"])
     quantum.submit()
 
-    # 5: Test - Login
+    # 4: Test - Login
     assert homeweb.wait_for_dashboard()
     homeweb.set_authenticated(True)
 
-# TODO: BAT-WEB-008
+def test_bat_web_008(homeweb):
+    assert homeweb.is_authenticated()
+    childcare_resource_target = "https://homeweb.ca/app/en/resources/579ba4db88db7af01fe6ddd4"
+    eldercare_resource_target = "https://homeweb.ca/app/en/resources/579ba49a88db7af01fe6ddc8"
+    hra_resource_target = "https://homeweb.ca/app/en/resources/579ba53088db7af01fe6dde6"
+
+    # 1: Test - ChildCare - Lifestage transfer kickout
+    homeweb.driver.get(childcare_resource_target)
+    assert homeweb.wait_for_resource_content()
+    homeweb.click_element("class name", "btn-primary")
+    assert homeweb.wait_for_lifestage_transfer()
+    homeweb.go_back()
+
+    # 2: Test - ElderCare - Lifestage transfer kickout
+    homeweb.driver.get(eldercare_resource_target)
+    assert homeweb.wait_for_resource_content()
+    homeweb.click_element("class name", "btn-primary")
+    assert homeweb.wait_for_lifestage_transfer()
+    homeweb.go_back()
+
+    # 3: Test - HRA - LifeStyles transfer kickout
+    homeweb.driver.get(hra_resource_target)
+    assert homeweb.wait_for_resource_content()
+    homeweb.click_element("class name", "btn-primary")
+    assert homeweb.wait_for_lifestyle_transfer()
+    homeweb.go_back()
+
 # TODO: BAT-WEB-009
 # TODO: BAT-WEB-010
 # TODO: BAT-WEB-011
