83bc520268b787627d342a3fb7d981868642a2ab
Mark De Guzman <mdguzman@homewoodhealth.com>
2026-02-18 14:48:39 -0700

    Support FR Tests
    BAT-WEB-007
    BAT-WEB-008
    BAT-WEB-009
    BAT-WEB-010

--- tests/bat_web_test.py
+++ tests/bat_web_test.py
@@ -5,14 +5,14 @@ from pages.Constants import HOMEWEB_DOMAIN, QUANTUM_API_DOMAIN
 def test_bat_web_001(homeweb):
     # 1: Test - Navigate to Homeweb landing
     homeweb.navigate_landing()
-    homeweb.set_landing(True)
     assert HOMEWEB_DOMAIN in homeweb.current_url.lower()
     f'Expected homeweb in URL, got: {homeweb.current_url}'
 
+
 def test_bat_web_002(homeweb):
     assert homeweb.is_landing()
-    resources = homeweb.landing["elements"]["resources"]
-    paths = homeweb.landing["paths"]["resources"]
+    resources = homeweb.public["elements"]["resources"]
+    paths = homeweb.public["paths"]["resources"]
 
     # 1: Test - Resource 1
     homeweb.click_element("xpath", resources["emotional_intelligence"])
@@ -38,9 +38,10 @@ def test_bat_web_002(homeweb):
     assert homeweb.wait_for_resource_content()
     homeweb.go_back()
 
+
 def test_bat_web_003(homeweb, quantum, credentials):
     assert homeweb.is_landing()
-    buttons = homeweb.landing["elements"]["buttons"]
+    buttons = homeweb.public["elements"]["buttons"]
 
     # 1: Test - Sign In - Button
     homeweb.click_element("xpath", buttons["sign_in"])
@@ -49,8 +50,7 @@ def test_bat_web_003(homeweb, quantum, credentials):
     # 2: Test - Login - Homeweb - Personal
     quantum.login(credentials["personal"]["email"], credentials["personal"]["password"])
     assert homeweb.wait_for_dashboard()
-    homeweb.set_landing(False)
-    homeweb.set_authenticated(True)
+
 
 def test_bat_web_004(homeweb):
     assert homeweb.is_authenticated()
@@ -59,6 +59,7 @@ def test_bat_web_004(homeweb):
     resource_target = homeweb.base_url + "/" + homeweb.language + "/user/articles/56252b81e40e6f50062aa714"
     homeweb.driver.get(resource_target)
     assert homeweb.wait_for_resource_content()
+
 
 def test_bat_web_005(homeweb):
     assert homeweb.is_authenticated()
@@ -73,6 +74,7 @@ def test_bat_web_005(homeweb):
     assert homeweb.wait_for_sentio_transfer()
     homeweb.go_back()
 
+
 def test_bat_web_006(homeweb):
     assert homeweb.is_authenticated()
     header = homeweb.header
@@ -84,101 +86,105 @@ def test_bat_web_006(homeweb):
 
     # 2: Test - Logout
     header.click_element("css selector", header_buttons["sign_out"])
-    homeweb.set_landing(True)
-    assert homeweb.is_landing()
-    homeweb.set_authenticated(False)
+    assert homeweb.wait_for_logout()
 
-# def test_bat_web_007(homeweb, quantum, credentials):
-#     assert homeweb.is_landing()
-#     header = homeweb.header
-#     header_buttons = header.elements["buttons"]
-#     paths = header.paths["buttons"]
-#
-#     # 1: Test - Sign In - Header
-#     header.click_element("class name", header_buttons["sign_in"])
-#     assert paths["sign_in"] in quantum.current_url.lower()
-#
-#     # 2: Test - Login - Homeweb - Demo
-#     quantum.login(credentials["demo"]["email"], credentials["demo"]["password"])
-#     assert homeweb.wait_for_dashboard()
-#     homeweb.set_authenticated(True)
-#
-# def test_bat_web_008(homeweb):
-#     assert homeweb.is_authenticated()
-#     childcare_resource_target = HOMEWEB_BASE_URL + "/app/en/resources/579ba4db88db7af01fe6ddd4"
-#     eldercare_resource_target = HOMEWEB_BASE_URL + "/app/en/resources/579ba49a88db7af01fe6ddc8"
-#     hra_resource_target = HOMEWEB_BASE_URL + "/app/en/resources/579ba53088db7af01fe6dde6"
-#
-#     # 1: Test - ChildCare - Lifestage transfer kickout
-#     homeweb.driver.get(childcare_resource_target)
-#     assert homeweb.wait_for_resource_content()
-#     homeweb.click_element("class name", "btn-primary")
-#     assert homeweb.wait_for_lifestage_transfer()
-#
-#     # 2: Test - ElderCare - Lifestage transfer kickout
-#     homeweb.driver.get(eldercare_resource_target)
-#     assert homeweb.wait_for_resource_content()
-#     homeweb.click_element("class name", "btn-primary")
-#     assert homeweb.wait_for_lifestage_transfer()
-#
-#     # 3: Test - HRA - LifeStyles transfer kickout
-#     homeweb.driver.get(hra_resource_target)
-#     assert homeweb.wait_for_resource_content()
-#     homeweb.click_element("class name", "btn-primary")
-#     assert homeweb.wait_for_lifestyle_transfer()
-#
-# def test_bat_web_009(homeweb):
-#     assert homeweb.is_authenticated()
-#     header = homeweb.header
-#     header_buttons = header.elements["buttons"]
-#
-#     # 1: Navigate to course
-#     course_target = HOMEWEB_BASE_URL + "/app/en/resources/564a36083392100756dd3e32"
-#     homeweb.driver.get(course_target)
-#     assert homeweb.wait_for_resource_content()
-#
-#     # 2: Test - Open modal
-#     homeweb.click_element("css selector", "[data-bs-toggle=\"modal\"]")
-#     assert homeweb.wait_for_modal()
-#
-#     # 3: Test - Dismiss modal, display course content
-#     homeweb.click_element("css selector", "[data-bs-dismiss=\"modal\"]")
-#     assert homeweb.wait_for_course_content()
-#     homeweb.wait_for_no_backdrop()
-#     homeweb.wait_for_layout_idle()
-#     homeweb.driver.switch_to.default_content()
-#     homeweb.wait.until(lambda d: d.execute_script("return document.readyState") == "complete")
-#
-#     # 4: Test - Menu dropdown
-#     header.click_element("class name", header_buttons["menu"])
-#     assert header.wait_for_account_menu()
-#
-#     # 5: Test - Logout
-#     header.click_element("css selector", header_buttons["sign_out"])
-#     assert "https://homeweb.ca/" in homeweb.current_url.lower()
-#     assert homeweb.is_landing()
-#     homeweb.set_authenticated(False)
-#
-# def test_bat_web_010(homeweb):
-#     resource_1_target = HOMEWEB_BASE_URL + "/summertime-and-your-health?embedded"
-#     resource_2_target = HOMEWEB_BASE_URL + "/mental-health-benefits-of-exercise?embedded"
-#     resource_3_target = HOMEWEB_BASE_URL + "/summer-beauty-from-the-inside-out?embedded"
-#
-#     # 1: Test - Embedded Resource - 1
-#     homeweb.driver.get(resource_1_target)
-#     assert homeweb.wait_for_resource_content()
-#     homeweb.go_back()
-#
-#     # 2: Test - Embedded Resource - 2
-#     homeweb.driver.get(resource_2_target)
-#     assert homeweb.wait_for_resource_content()
-#     homeweb.go_back()
-#
-#     # 3: Test - Embedded Resource - 3
-#     homeweb.driver.get(resource_3_target)
-#     assert homeweb.wait_for_resource_content()
-#     homeweb.go_back()
-#
+    # KNOWN ISSUE 1 - Workaround: Manually navigate back to landing (locale-aware)
+    homeweb.navigate_landing()
+
+
+def test_bat_web_007(homeweb, quantum, credentials):
+    assert homeweb.is_landing()
+    header = homeweb.header
+    header_buttons = header.elements["buttons"]
+    paths = header.paths["buttons"]
+
+    # 1: Test - Sign In - Header
+    header.click_element("class name", header_buttons["sign_in"])
+    assert paths["sign_in"] in quantum.current_url.lower()
+
+    # 2: Test - Login - Homeweb - Demo
+    quantum.login(credentials["demo"]["email"], credentials["demo"]["password"])
+    assert homeweb.wait_for_dashboard()
+
+
+def test_bat_web_008(homeweb):
+    assert homeweb.is_authenticated()
+    childcare_resource_target = homeweb.base_url + "/app/" + homeweb.language + "/resources/579ba4db88db7af01fe6ddd4"
+    eldercare_resource_target = homeweb.base_url + "/app/" + homeweb.language + "/resources/579ba49a88db7af01fe6ddc8"
+    hra_resource_target = homeweb.base_url + "/app/" + homeweb.language + "/resources/579ba53088db7af01fe6dde6"
+
+    # 1: Test - ChildCare - Lifestage transfer kickout
+    homeweb.driver.get(childcare_resource_target)
+    assert homeweb.wait_for_resource_content()
+    homeweb.click_element("class name", "btn-primary")
+    assert homeweb.wait_for_lifestage_transfer()
+
+    # 2: Test - ElderCare - Lifestage transfer kickout
+    homeweb.driver.get(eldercare_resource_target)
+    assert homeweb.wait_for_resource_content()
+    homeweb.click_element("class name", "btn-primary")
+    assert homeweb.wait_for_lifestage_transfer()
+
+    # 3: Test - HRA - LifeStyles transfer kickout
+    homeweb.driver.get(hra_resource_target)
+    assert homeweb.wait_for_resource_content()
+    homeweb.click_element("class name", "btn-primary")
+    assert homeweb.wait_for_lifestyle_transfer()
+
+
+def test_bat_web_009(homeweb):
+    assert homeweb.is_authenticated()
+    header = homeweb.header
+    header_buttons = header.elements["buttons"]
+
+    # 1: Navigate to course
+    course_target = homeweb.base_url + "/app/" + homeweb.language + "/resources/564a36083392100756dd3e32"
+    homeweb.driver.get(course_target)
+    assert homeweb.wait_for_resource_content()
+
+    # 2: Test - Open modal
+    homeweb.click_element("css selector", "[data-bs-toggle=\"modal\"]")
+    assert homeweb.wait_for_modal()
+
+    # 3: Test - Dismiss modal, display course content
+    homeweb.click_element("css selector", "[data-bs-dismiss=\"modal\"]")
+    # assert homeweb.wait_for_course_content()
+    assert homeweb.wait_for_course_content(), "iframe content issue"
+
+    # 4: Test - Menu dropdown
+    header.click_element("class name", header_buttons["menu"])
+    assert header.wait_for_account_menu(), "Menu not found"
+
+    # 5: Test - Logout
+    header.click_element("css selector", header_buttons["sign_out"])
+    assert homeweb.wait_for_logout()
+
+    # KNOWN ISSUE 1 - Workaround: Manually navigate back to landing (locale-aware)
+    homeweb.navigate_landing()
+
+
+def test_bat_web_010(homeweb):
+    lang_prefix = "" if homeweb.language.lower() == "en" else f"/{homeweb.language}"
+
+    resource_1_target = homeweb.base_url + lang_prefix + "/summertime-and-your-health?embedded"
+    resource_2_target = homeweb.base_url + lang_prefix + "/mental-health-benefits-of-exercise?embedded"
+    resource_3_target = homeweb.base_url + lang_prefix + "/summer-beauty-from-the-inside-out?embedded"
+
+    # 1: Test - Embedded Resource - 1
+    homeweb.driver.get(resource_1_target)
+    assert homeweb.wait_for_resource_content()
+    homeweb.go_back()
+
+    # 2: Test - Embedded Resource - 2
+    homeweb.driver.get(resource_2_target)
+    assert homeweb.wait_for_resource_content()
+    homeweb.go_back()
+
+    # 3: Test - Embedded Resource - 3
+    homeweb.driver.get(resource_3_target)
+    assert homeweb.wait_for_resource_content()
+    homeweb.go_back()
+
 # def test_bat_web_011(homeweb, quantum, customer_portal, credentials):
 #     assert homeweb.is_landing()
 #     paths = homeweb.landing["paths"]["buttons"]
