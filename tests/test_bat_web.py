bc6e082cf86836d2dcd02d98161d3ba21e9ba343
Mark De Guzman <mdguzman@homewoodhealth.com>
2026-02-27 11:20:02 -0700

    [BAT] - Refactor to support sub-suite test files

---
+++ tests/test_bat_web.py
@@ -0,0 +1,11 @@
+################# BUILD ACCEPTANCE ################ [WIP]
+##################### HOMEWEB ##################### [DONE]
+################# CUSTOMER PORTAL ################# [DONE]
+############## SENTIO BETA - CLIENT ############### [WIP]
+############# SENTIO BETA - PROVIDER ############## [DONE]
+
+from test_homeweb import *
+from test_customer_portal import *
+from test_sentio_beta_client import *
+from test_sentio_beta_provider import *
+
