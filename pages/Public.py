0c9793fa977c3f1af132444eb3811c29c3d8865d
Mark De Guzman <mdguzman@homewoodhealth.com>
2026-02-27 11:21:25 -0700

    [BAT-HOMEWEB] - Temp fix for BAT-WEB-002
    Articles changed, currently working on a fix to support Homepage articles dynamically

--- pages/Public.py
+++ pages/Public.py
@@ -2,9 +2,9 @@ class Public:
     EN = {
         "elements": {
             "resources": {
+                "self_compassion": "//a[@title='Self-Compassion: Steps you can take to improve your mental health']",
+                "neurodiversity": "//a[@title='Neurodiversity in Focus: The Energy Drain of Masking ']",
                 "emotional_intelligence": "//a[@title='Emotional Intelligence: Why it’s Important and How to Build it']",
-                "anxiety": "//a[@title='Anxiety: Strategies for Coping with Change and Uncertainty']",
-                "letting_go": "//a[@title='Letting Go of Expectations']",
                 "toolkit": "View the Mental Health Toolkit",
             },
             "buttons": {
@@ -13,13 +13,10 @@ class Public:
         },
         "paths": {
             "resources": {
-                "emotional_intelligence": "/en/homewood/neurodiversity-focus-energy-drain-masking",
-                "anxiety": "/en/homewood/emotional-intelligence-why-its-important-how-build",
-                "letting_go": "/en/homewood/ai-anxiety-strategies-coping-with-change-uncertainty",
+                "self_compassion": "/en/homewood/self-compassion-steps-to-improve-your-mental-health",
+                "neurodiversity": "/en/homewood/neurodiversity-focus-energy-drain-masking",
+                "emotional_intelligence": "/en/homewood/emotional-intelligence-why-its-important-how-build",
                 "toolkit": "/en/homewood/navigating-uncertainty-your-mental-health-toolkit",
-                "summertime": "/summertime-and-your-health?embedded",
-                "mental_health": "/mental-health-benefits-of-exercise?embedded",
-                "summer_beauty": "/summer-beauty-from-the-inside-out?embedded"
             },
             "buttons": {
                 "sign_in": "/en/login"
@@ -30,9 +27,9 @@ class Public:
     FR = {
         "elements": {
             "resources": {
+                "self_compassion": "//a[@title=' Autocompassion : des gestes concrets pour soutenir votre santé mentale']",
+                "neurodiversity": "//a[@title='La neurodiversité en point de mire : la perte d’énergie liée à la dissimulation ']",
                 "emotional_intelligence": "//a[@title='L’intelligence émotionnelle : pourquoi est-elle importante et comment la développer ']",
-                "anxiety": "//a[@title='L’anxiété liée à l’IA : stratégies pour faire face au changement et à l’incertitude']",
-                "letting_go": "//a[@title='Lâcher prise sur ses attentes']",
                 "toolkit": "Consulter la boîte à outils sur la santé mentale",
             },
             "buttons": {
@@ -41,13 +38,10 @@ class Public:
         },
         "paths": {
             "resources": {
-                "emotional_intelligence": "/fr/homewood/la-neurodiversite-en-point-de-mire",
-                "anxiety": "/fr/homewood/lintelligence-emotionnelle-pourquoi-estelle-importante-et-comment-la-developper",
-                "letting_go": "/fr/homewood/lanxiete-liee-lia-strategies-pour-faire-face-au-changement-et-lincertitude",
+                "self_compassion": "/fr/homewood/autocompassion-pour-soutenir-votre-sante-mentale",
+                "neurodiversity": "/fr/homewood/la-neurodiversite-en-point-de-mire",
+                "emotional_intelligence": "/fr/homewood/lintelligence-emotionnelle-pourquoi-estelle-importante-et-comment-la-developper",
                 "toolkit": "/fr/homewood/gerer-l-incertitude-votre-boite-a-outils-sur-la-sante-mentale",
-                "summertime": "/summertime-and-your-health?embedded",
-                "mental_health": "/mental-health-benefits-of-exercise?embedded",
-                "summer_beauty": "/summer-beauty-from-the-inside-out?embedded"
             },
             "buttons": {
                 "sign_in": "/fr/login"
