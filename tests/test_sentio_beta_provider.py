53654b2b4887932f487501133977b15f4e317846
Mark De Guzman <mdguzman@homewoodhealth.com>
2026-03-03 16:40:03 -0700

    [BAT-SENTIO-BETA-PROVIDER] - Update Comments

--- tests/test_sentio_beta_provider.py
+++ tests/test_sentio_beta_provider.py
@@ -3,16 +3,18 @@
 from selenium.webdriver.common.by import By
 
 
+# TEST: Navigate Sentio Beta - Provider
 def test_bat_web_025(sentio_beta_provider, quantum):
     sentio_beta_provider.driver.get(sentio_beta_provider.base_url)
     assert quantum.base_url in sentio_beta_provider.current_url.lower()
 
 
+# TEST: Sentio Beta Provider Login
 def test_bat_web_026(sentio_beta_provider, quantum, credentials):
     quantum.login(credentials["personal"]["email"], credentials["personal"]["password"])
     assert sentio_beta_provider.wait_for_login()
 
-
+# TEST: New Dashboard
 def test_bat_web_027(sentio_beta_provider):
     assert sentio_beta_provider._is_authenticated
 
@@ -27,11 +29,10 @@ def test_bat_web_027(sentio_beta_provider):
     header.click_element(By.CSS_SELECTOR, header_elements["new"])
     assert sentio_beta_provider.wait_for_dashboard("new")
 
-
+# TEST: Sentio Beta Provider Logout
 def test_bat_web_028(sentio_beta_provider, quantum):
     assert sentio_beta_provider._is_authenticated
 
-    # 1: Test - Logout
     header = sentio_beta_provider.header
     header_buttons = header.elements["buttons"]
     header.click_element(By.CLASS_NAME, header_buttons["menu"])
