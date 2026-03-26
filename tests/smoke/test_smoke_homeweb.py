e4a7fb3540601df7d5ebed3306843d1a7fe71df6
Mark De Guzman <mdguzman@homewoodhealth.com>
2026-03-26 12:43:10 -0600

    [SMOKE] - Initial Wiring of Smoke Test Suite
    - Add Homeweb Smoke Suite and TODOs for tests according to spreadsheet

---
+++ tests/smoke/test_smoke_homeweb.py
@@ -0,0 +1,43 @@
+# Copyright © 2026 - Homewood Health Inc.
+################# SMOKE ################
+################ HOMEWEB ###############
+
+# TODO: SMOKE-001 | Homewood Registration - Initial State
+# TODO: SMOKE-002	| Create an account
+# TODO: SMOKE-003	| Homeweb Landing - Initial State
+# TODO: SMOKE-004	| Dashboard - Initial State
+# TODO: SMOKE-005	| Initial State
+# TODO: SMOKE-006	| PulseCheck Reccommendation - Check In
+# TODO: SMOKE-007	| Initial State
+# TODO: SMOKE-008	| Launch Pathfinder
+# TODO: SMOKE-009	| Assessment Start
+# TODO: SMOKE-010	| 5 star rating pages
+# TODO: SMOKE-011	| Intake questions
+# TODO: SMOKE-012	| Booking calendar
+# TODO: SMOKE-013	| Booking calendar
+# TODO: SMOKE-014	| More options
+# TODO: SMOKE-015	| Booking calendar
+# TODO: SMOKE-016	| Booking calendar
+# TODO: SMOKE-017	| Enbridge Landing - Initial State
+# TODO: SMOKE-018	| Suncor Landing - Initial State
+# TODO: SMOKE-019	| LSO Landing- Initial State
+# TODO: SMOKE-020	| EQ Landing- Initial State
+# TODO: SMOKE-021	| ALUMNI Landing- Initial State
+# TODO: SMOKE-022	| Pacific Blue Cross Landing- Initial State
+# TODO: SMOKE-023	| External redirects for homeweb
+# TODO: SMOKE-024	| External redirects for homeweb LSO
+# TODO: SMOKE-025	| External redirects for homeweb Enbridge
+# TODO: SMOKE-026	| External redirects for homeweb EQ
+# TODO: SMOKE-027	| External redirects for homeweb Suncor
+# TODO: SMOKE-028	| External redirects for homeweb PBC
+# TODO: SMOKE-029	| Initial page state
+# TODO: SMOKE-030	| Message Received
+# TODO: SMOKE-031	| Message Received
+# TODO: SMOKE-032	| Message Received
+# TODO: SMOKE-033	| Send Message
+# TODO: SMOKE-034	| Sync
+# TODO: SMOKE-035	| Change language
+# TODO: SMOKE-036	| Login in test
+# TODO: SMOKE-037	| Forgot password
+# TODO: SMOKE-038	| Footer tests
+# TODO: SMOKE-039	| Logout 
---
+++ tests/smoke/test_smoke_web.py
@@ -0,0 +1,7 @@
+# Copyright © 2026 - Homewood Health Inc.
+
+################# SMOKE TEST SUITE ################
+################# QUANTUM API #####################
+################# HOMEWEB #########################
+
+from test_smoke_homeweb import *
