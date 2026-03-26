eb41fd32b4ae351a38a0214252477a98ad080c19
Mark De Guzman <mdguzman@homewoodhealth.com>
2026-03-26 12:47:12 -0600

    [SMOKE-HW] - Update comments

--- tests/smoke/test_smoke_homeweb.py
+++ tests/smoke/test_smoke_homeweb.py
@@ -2,12 +2,21 @@
 ################# SMOKE ################
 ################ HOMEWEB ###############
 
+# REGISTRATION
 # TODO: SMOKE-001 | Homewood Registration - Initial State
 # TODO: SMOKE-002	| Create an account
+
+# LANDING PAGE - Homeweb Anonymouse
 # TODO: SMOKE-003	| Homeweb Landing - Initial State
+
+# DASHBOARD
 # TODO: SMOKE-004	| Dashboard - Initial State
+
+# PULSECHECK
 # TODO: SMOKE-005	| Initial State
 # TODO: SMOKE-006	| PulseCheck Reccommendation - Check In
+
+# PATHFINDER
 # TODO: SMOKE-007	| Initial State
 # TODO: SMOKE-008	| Launch Pathfinder
 # TODO: SMOKE-009	| Assessment Start
@@ -18,18 +27,24 @@
 # TODO: SMOKE-014	| More options
 # TODO: SMOKE-015	| Booking calendar
 # TODO: SMOKE-016	| Booking calendar
+
+# LANDING PAGE - Other
 # TODO: SMOKE-017	| Enbridge Landing - Initial State
 # TODO: SMOKE-018	| Suncor Landing - Initial State
 # TODO: SMOKE-019	| LSO Landing- Initial State
 # TODO: SMOKE-020	| EQ Landing- Initial State
 # TODO: SMOKE-021	| ALUMNI Landing- Initial State
 # TODO: SMOKE-022	| Pacific Blue Cross Landing- Initial State
+
+# RESOURCES
 # TODO: SMOKE-023	| External redirects for homeweb
 # TODO: SMOKE-024	| External redirects for homeweb LSO
 # TODO: SMOKE-025	| External redirects for homeweb Enbridge
 # TODO: SMOKE-026	| External redirects for homeweb EQ
 # TODO: SMOKE-027	| External redirects for homeweb Suncor
 # TODO: SMOKE-028	| External redirects for homeweb PBC
+
+# MESSAGES
 # TODO: SMOKE-029	| Initial page state
 # TODO: SMOKE-030	| Message Received
 # TODO: SMOKE-031	| Message Received
@@ -37,7 +52,9 @@
 # TODO: SMOKE-033	| Send Message
 # TODO: SMOKE-034	| Sync
 # TODO: SMOKE-035	| Change language
+
+# IDENTITY
 # TODO: SMOKE-036	| Login in test
 # TODO: SMOKE-037	| Forgot password
 # TODO: SMOKE-038	| Footer tests
-# TODO: SMOKE-039	| Logout 
+# TODO: SMOKE-039	| Logout
